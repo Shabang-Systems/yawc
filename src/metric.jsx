deb7fdace2023b2fb480a335cdb51119cc5c69c0
Houjun Liu <houjun@jemoka.com>
2024-08-04 12:18:22 -0700

    aaaaand done.

--- src/metric.jsx
+++ src/metric.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, Suspense, useContext, useTransition, useRef, useCallback } from 'react';
-import { Button, SafeAreaView, Text, View, FlatList, TouchableOpacity, Platform, processColor } from "react-native";
+import { Button, SafeAreaView, Text, View, FlatList, TouchableOpacity, Pressable, Platform, processColor } from "react-native";
 import { usePreloadedQuery, useQueryLoader, usePaginationFragment } from 'react-relay';
 
 import { Divider } from '@rneui/themed';
@@ -15,6 +15,7 @@ import Load from "../components/load.jsx";
 import RunStatus from "../components/runstatus.jsx";
 import {LineChart} from 'react-native-charts-wrapper';
 
+import Slider from '@react-native-community/slider';
 import moment from "moment";
 
 import Styling from "./styles.js";
@@ -25,18 +26,54 @@ import * as ScreenOrientation from 'expo-screen-orientation';
 
 const getKey = (raw) => Object.keys(raw[0]).filter(x => x != "_step")[0];
 
-function BuildDataset(raw) {
+// https://stackoverflow.com/questions/20811131/javascript-remove-outlier-from-an-array
+function calculateOutliers(someArray) {  
+    var values = someArray.concat();
+    values.sort( function(a, b) {
+            return a - b;
+         });
+    var q1 = values[Math.floor((values.length / 4))];
+    var q3 = values[Math.ceil((values.length * (3 / 4)))];
+    var iqr = q3 - q1;
+    var maxValue = q3 + iqr*1;
+    var minValue = q1 - iqr*1;
+    return [maxValue, minValue];
+}
+
+// https://stackoverflow.com/questions/40057020/calculating-exponential-moving-average-ema-using-javascript
+function EMACalc(mArray,mRange) {
+    let k = 2/(mRange + 1);
+    // first item is just the same as the first item in the input
+    let emaArray = [mArray[0]];
+    // for the rest of the items, they are computed with the previous one
+    for (var i = 1; i < mArray.length; i++) {
+        emaArray.push(mArray[i] * k + emaArray[i - 1] * (1 - k));
+    }
+    return emaArray;
+}
+
+function BuildDataset(raw, ignoreOutliers, ema) {
     let steps = [];
     let values = [];
     let ticks = [];
 
     let name = getKey(raw);
+
+    let [max, min] = calculateOutliers(raw.map(x=>x[name]));
+
     for (const i of raw) {
         let step = i["_step"];
         let value = i[name];
-        ticks.push(step);
-        values.push(value);
-        steps.push({x: step, y: value});
+        if (!ignoreOutliers || (value >= min && value <= max)) {
+            ticks.push(step);
+            values.push(value);
+            steps.push({x: step, y: value});
+        }
+    }
+
+    if (ema != 0) {
+        values = EMACalc(values, ema);
+        steps = steps.map((i, indx) => ({x: i.x, y: values[indx]}));
     }
 
     return {ds: steps, steps: ticks, values, name};
@@ -54,10 +91,16 @@ function range(x) {
 
 function MetricView({ navigation, qr, run }) {
     let data = usePreloadedQuery(HistoryQuery, qr);
-    let {ds, steps, values, name} = BuildDataset(data.project.run.sampledHistory[0]);
+
+    let [ignoreOutliers, setIgnoreOutliers] = useState(false);
+    let [ema, setEMA] = useState(0);
+
+    let {ds, steps, values, name} = BuildDataset(data.project.run.sampledHistory[0],
+                                                 ignoreOutliers, ema);
     
     return (
-        <View style={{backgroundColor: "white", height: "100%"}}>
+        <View style={{backgroundColor: "white", height: "100%",
+                      padding: 20, paddingBottom: 0, paddingTop: 25}}>
             <LineChart
                 legend={{verticalAlignment: "TOP"}}
                 style={{flex: 1}}
@@ -82,9 +125,46 @@ function MetricView({ navigation, qr, run }) {
                     }
                 }]}}
             />
-            <View style={{margin: 10, marginTop: 20, height: "6%"}}>
-                <Text>we were youngggg bb</Text>
-                <Button title="no" onPress={() => {navigation.goBack();}}/>
+            <View style={{margin: 10, marginTop: 10}}>
+                <View style={{display: "flex",
+                              padding: 20,
+                              paddingBottom: 5,
+                              flexDirection: "row",
+                                alignItems: "center",
+                              justifyContent: "space-between"}}>
+                    <View style={{display: "flex",
+                                  flexDirection: "row",
+                                  alignItems: "center",
+                                  justifyContent: "flex-start"}}>
+                        <Pressable
+                            style={{backgroundColor: ignoreOutliers ? "#9c9c9c" : "#f0f0f0",
+                                    borderRadius: 5,
+                                    padding: 5,
+                                    display: "inline-block"}}
+                            onPress={() => {setIgnoreOutliers(!ignoreOutliers);}}>
+                            <Ionicons name="chevron-expand"
+                                      color={ignoreOutliers ? "white" : "#4d4d4d"}
+                                      size={17}/>
+                        </Pressable>
+                        
+                        <Slider
+                            style={{width: "60%", height: 3, marginLeft: 15}}
+                            minimumValue={0}
+                            maximumValue={50}
+                            step={1}
+                            onValueChange={(v) => setEMA(v)}
+                            value={ema}
+                        />
+                    </View>
+                    <TouchableOpacity
+                        style={{backgroundColor: "#f0f0f0",
+                                borderRadius: 5,
+                                padding: 5,
+                                display: "inline-block"}}
+                        onPress={() => {navigation.goBack();}}>
+                        <Ionicons name="close" color={"#4d4d4d"} size={17}/>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
